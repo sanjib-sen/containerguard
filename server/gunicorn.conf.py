fb24eee45053c230398b82f3428fb1f34fe3e011
jsaenz34 <jackson.saenz@tamucc.edu>
2026-03-30 08:43:41 -0500

    added the shared dir between uvicorn workers

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -1,5 +1,16 @@
 import multiprocessing
 import os
+import shutil
+import tempfile
+
+from prometheus_client import multiprocess
+
+
+PROMETHEUS_MULTIPROC_DIR = os.getenv(
+    "PROMETHEUS_MULTIPROC_DIR",
+    os.path.join(tempfile.gettempdir(), "containerguard-prometheus"),
+)
+os.environ["PROMETHEUS_MULTIPROC_DIR"] = PROMETHEUS_MULTIPROC_DIR
 
 
 bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
@@ -10,3 +21,13 @@ errorlog = "-"
 graceful_timeout = 30
 timeout = 30
 keepalive = 30
+
+
+def on_starting(server):
+    shutil.rmtree(PROMETHEUS_MULTIPROC_DIR, ignore_errors=True)
+    os.makedirs(PROMETHEUS_MULTIPROC_DIR, exist_ok=True)
+    server.log.info("initialized Prometheus multiprocess dir at %s", PROMETHEUS_MULTIPROC_DIR)
+
+
+def child_exit(server, worker):
+    multiprocess.mark_process_dead(worker.pid, path=PROMETHEUS_MULTIPROC_DIR)
